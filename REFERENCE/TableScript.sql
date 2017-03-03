2e44e78ce35fa8cb92ad16544b82e28a6e6ebbc3
java90@Durad <Durad.han@gmail.com>
2017-03-03 16:41:05 +0900

    테이블 스크립트 작성
    (샘플이 포함되어있으니 전체선택하여 실행하고 각자 작성하시면 됩니다)

--- REFERENCE/TableScript.sql
+++ REFERENCE/TableScript.sql
@@ -27,8 +27,8 @@ create table tb_Around_Card (
 	Card_Content		varchar2(420)	not null,				-- 카드 내용 (140글자)
 	Card_Feeling		char(1) 		not null,				-- 카드 기분 (1:, 2:, 3:, 4:, 5: )
 	Card_Hashtag		varchar2(420) 	not null,				-- 카드에 작성된 해시태그 ( #.. ; #.. ) ; 을 사용하여 구분
-	Card_Longitude		number(5,9)		not null,				-- 카드 마커 경도값
-	Card_Latitude		number(5,9)		not null,				-- 카드 마커 위도값
+	Card_Longitude		number(14,9)	not null,				-- 카드 마커 경도값
+	Card_Latitude		number(14,9)	not null,				-- 카드 마커 위도값
 	Card_Img_Path		varchar2(200),							-- 카드 배경 이미지 주소값
 	Card_Reg_Date		varchar2(20) 	default to_char(sysdate)
 );
@@ -112,4 +112,119 @@ create table tb_Around_Img (
 -----------------------------------------------------------------------------------------------------------------
 --  테이블 Sample 입력
 -----------------------------------------------------------------------------------------------------------------
+
+-- user1 : admin, user2~4 : tester
+insert	into tb_Around_User (user_seq, user_id, user_pass)
+		values (s_Around_User.nextval, 'admin', 'admin');
+insert	into tb_Around_User (user_seq, user_id, user_pass)
+		values (s_Around_User.nextval, 'tester01', 'tester01');
+insert	into tb_Around_User (user_seq, user_id, user_pass)
+		values (s_Around_User.nextval, 'tester02', 'tester02');
+insert	into tb_Around_User (user_seq, user_id, user_pass)
+		values (s_Around_User.nextval, 'tester03', 'tester03');
+		
+-- card1~7 : card card card	
+insert	into tb_Around_Card (card_seq, user_seq, card_content, card_feeling, card_hashtag, CARD_LONGITUDE, 	CARD_LATITUDE )
+		values (s_Around_Card.nextval, 1, '첫번째 카드는 어드민이 작성했답니다', 1, '#첫글 #첫카드 #샘플임', 1111.1111, 2222.2222);
+insert	into tb_Around_Card (card_seq, user_seq, card_content, card_feeling, card_hashtag, CARD_LONGITUDE, 	CARD_LATITUDE )
+		values (s_Around_Card.nextval, 2, '두번째 카드는 테스터가 작성했답니다', 1, '#야호 #샘플임', 1111.1111, 1111.1111);
+insert	into tb_Around_Card (card_seq, user_seq, card_content, card_feeling, card_hashtag, CARD_LONGITUDE, 	CARD_LATITUDE )
+		values (s_Around_Card.nextval, 2, '세번째 카드도 테스터가 작성했답니다', 1, '#야호 #샘플임', 1111.1111, 1111.1111);
+insert	into tb_Around_Card (card_seq, user_seq, card_content, card_feeling, card_hashtag, CARD_LONGITUDE, 	CARD_LATITUDE )
+		values (s_Around_Card.nextval, 3, '네번째라고 별다를 거 없다', 3, '#샘플', 1111.1111, 1111.1111);
+insert	into tb_Around_Card (card_seq, user_seq, card_content, card_feeling, card_hashtag, CARD_LONGITUDE, 	CARD_LATITUDE )
+		values (s_Around_Card.nextval, 4, '야 인마!', 5, '#샘플 #버럭', 1111.1111, 1111.1111);
+insert	into tb_Around_Card (card_seq, user_seq, card_content, card_feeling, card_hashtag, CARD_LONGITUDE, 	CARD_LATITUDE )
+		values (s_Around_Card.nextval, 4, '여기서부터는', 5, '#샘플 #설명충', 1111.1111, 1111.1111);		
+insert	into tb_Around_Card (card_seq, user_seq, card_content, card_feeling, card_hashtag, CARD_LONGITUDE, 	CARD_LATITUDE )
+		values (s_Around_Card.nextval, 4, '더미 데이터다', 5, '#샘플 #더미', 1111.1111, 1111.1111);		
+
+-- card	8~16 : dummy	
+insert	into tb_Around_Card (card_seq, user_seq, card_content, card_feeling, card_hashtag, CARD_LONGITUDE, 	CARD_LATITUDE )
+		values (s_Around_Card.nextval, 4, '더미더미더미더미더미더미더미더미', 1, '#샘플 #더미', 1111.1111, 1111.1111);		
+insert	into tb_Around_Card (card_seq, user_seq, card_content, card_feeling, card_hashtag, CARD_LONGITUDE, 	CARD_LATITUDE )
+		values (s_Around_Card.nextval, 4, '더미더미더미더미더미더미더미더미', 2, '#샘플 #더미', 1111.1111, 1111.1111);		
+insert	into tb_Around_Card (card_seq, user_seq, card_content, card_feeling, card_hashtag, CARD_LONGITUDE, 	CARD_LATITUDE )
+		values (s_Around_Card.nextval, 4, '더미더미더미더미더미더미더미더미', 3, '#샘플 #더미데이터', 1111.1111, 1111.1111);		
+insert	into tb_Around_Card (card_seq, user_seq, card_content, card_feeling, card_hashtag, CARD_LONGITUDE, 	CARD_LATITUDE )
+		values (s_Around_Card.nextval, 4, '더미더미더미더미더미더미더미더미', 4, '#샘플 #더미데이터', 1111.1111, 1111.1111);		
+insert	into tb_Around_Card (card_seq, user_seq, card_content, card_feeling, card_hashtag, CARD_LONGITUDE, 	CARD_LATITUDE )
+		values (s_Around_Card.nextval, 4, '더미더미더미더미더미더미더미더미', 5, '#샘플 #더미데이터', 1111.1111, 1111.1111);		
+insert	into tb_Around_Card (card_seq, user_seq, card_content, card_feeling, card_hashtag, CARD_LONGITUDE, 	CARD_LATITUDE )
+		values (s_Around_Card.nextval, 4, '더미더미더미더미더미더미더미더미', 5, '#샘플 #더미데이터', 1111.1111, 1111.1111);		
+insert	into tb_Around_Card (card_seq, user_seq, card_content, card_feeling, card_hashtag, CARD_LONGITUDE, 	CARD_LATITUDE )
+		values (s_Around_Card.nextval, 4, '더미더미더미더미더미더미더미더미', 5, '#샘플 #더미데이터', 1111.1111, 1111.1111);		
+insert	into tb_Around_Card (card_seq, user_seq, card_content, card_feeling, card_hashtag, CARD_LONGITUDE, 	CARD_LATITUDE )
+		values (s_Around_Card.nextval, 4, '더미더미더미더미더미더미더미더미', 5, '#샘플 #더미데이터', 1111.1111, 1111.1111);		
+insert	into tb_Around_Card (card_seq, user_seq, card_content, card_feeling, card_hashtag, CARD_LONGITUDE, 	CARD_LATITUDE )
+		values (s_Around_Card.nextval, 4, '더미더미더미더미더미더미더미더미', 5, '#샘플 #더미데이터', 1111.1111, 1111.1111);		
+		
+-- hashtag : #첫글 #첫카드 #샘플임(3) #샘플(13) #야호(2) #버럭 #설명충 #더미(3) #더미데이터(7)
+insert	into TB_AROUND_HASHTAG (hashtag_seq, Hashtag_Content, Hashtag_Frequency)
+		values (s_AROUND_HASHTAG.nextval, '첫글', 1);
+insert	into TB_AROUND_HASHTAG (hashtag_seq, Hashtag_Content, Hashtag_Frequency)
+		values (s_AROUND_HASHTAG.nextval, '첫카드', 1);
+insert	into TB_AROUND_HASHTAG (hashtag_seq, Hashtag_Content, Hashtag_Frequency)
+		values (s_AROUND_HASHTAG.nextval, '샘플임', 3);
+insert	into TB_AROUND_HASHTAG (hashtag_seq, Hashtag_Content, Hashtag_Frequency)
+		values (s_AROUND_HASHTAG.nextval, '샘플', 13);
+insert	into TB_AROUND_HASHTAG (hashtag_seq, Hashtag_Content, Hashtag_Frequency)
+		values (s_AROUND_HASHTAG.nextval, '야호', 2);
+insert	into TB_AROUND_HASHTAG (hashtag_seq, Hashtag_Content, Hashtag_Frequency)
+		values (s_AROUND_HASHTAG.nextval, '버럭', 1);
+insert	into TB_AROUND_HASHTAG (hashtag_seq, Hashtag_Content, Hashtag_Frequency)
+		values (s_AROUND_HASHTAG.nextval, '설명충', 1);
+insert	into TB_AROUND_HASHTAG (hashtag_seq, Hashtag_Content, Hashtag_Frequency)
+		values (s_AROUND_HASHTAG.nextval, '더미', 3);
+insert	into TB_AROUND_HASHTAG (hashtag_seq, Hashtag_Content, Hashtag_Frequency)
+		values (s_AROUND_HASHTAG.nextval, '더미데이터', 7);
+		
+-- 	comments	
+insert	into tb_Around_Comment (Comment_Seq, User_Seq, Card_Seq, Comment_Content)
+		values (s_Around_Comment.nextval, 2, 1, '와~ 첫글이당');
+insert	into tb_Around_Comment (Comment_Seq, User_Seq, Card_Seq, Comment_Content)
+		values (s_Around_Comment.nextval, 3, 1, '와~ 첫글이당ㅎ');
+insert	into tb_Around_Comment (Comment_Seq, User_Seq, Card_Seq, Comment_Content)
+		values (s_Around_Comment.nextval, 4, 1, '와~ 첫글이당ㅋ');
+insert	into tb_Around_Comment (Comment_Seq, User_Seq, Card_Seq, Comment_Content)
+		values (s_Around_Comment.nextval, 1, 1, '자설리');
+insert	into tb_Around_Comment (Comment_Seq, User_Seq, Card_Seq, Comment_Content)
+		values (s_Around_Comment.nextval, 1, 1, '설얍');
+insert	into tb_Around_Comment (Comment_Seq, User_Seq, Card_Seq, Comment_Content)
+		values (s_Around_Comment.nextval, 2, 2, '콩까지마');
+insert	into tb_Around_Comment (Comment_Seq, User_Seq, Card_Seq, Comment_Content)
+		values (s_Around_Comment.nextval, 2, 2, '콩까지마');
+insert	into tb_Around_Comment (Comment_Seq, User_Seq, Card_Seq, Comment_Content)
+		values (s_Around_Comment.nextval, 3, 2, '콩까지마!');
+insert	into tb_Around_Comment (Comment_Seq, User_Seq, Card_Seq, Comment_Content)
+		values (s_Around_Comment.nextval, 3, 2, '콩까지마!');
+insert	into tb_Around_Comment (Comment_Seq, User_Seq, Card_Seq, Comment_Content)
+		values (s_Around_Comment.nextval, 4, 2, '콩까지마!!');
+insert	into tb_Around_Comment (Comment_Seq, User_Seq, Card_Seq, Comment_Content)
+		values (s_Around_Comment.nextval, 4, 2, '콩까지마!!');
+		
+-- my
+insert	into tb_Around_My_Favorite (My_Seq, User_Seq, My_Content, My_Hashtag, My_Feeling)
+		values (s_Around_My_Favorite.nextval, 2, '1번글', '#태그1', 1);
+insert	into tb_Around_My_Favorite (My_Seq, User_Seq, My_Content, My_Hashtag, My_Feeling)
+		values (s_Around_My_Favorite.nextval, 3, '2번글', '#태그1', 2);
+insert	into tb_Around_My_Favorite (My_Seq, User_Seq, My_Content, My_Hashtag, My_Feeling)
+		values (s_Around_My_Favorite.nextval, 2, '3번글', '#태그1', 3);
+insert	into tb_Around_My_Favorite (My_Seq, User_Seq, My_Content, My_Hashtag, My_Feeling)
+		values (s_Around_My_Favorite.nextval, 4, '4번글', '#태그2', 4);
+insert	into tb_Around_My_Favorite (My_Seq, User_Seq, My_Content, My_Hashtag, My_Feeling)
+		values (s_Around_My_Favorite.nextval, 4, '5번글', '#태그2', 5);
+insert	into tb_Around_My_Favorite (My_Seq, User_Seq, My_Content, My_Hashtag, My_Feeling)
+		values (s_Around_My_Favorite.nextval, 3, '6번글', '#태그2', 2);
+insert	into tb_Around_My_Favorite (My_Seq, User_Seq, My_Content, My_Hashtag, My_Feeling)
+		values (s_Around_My_Favorite.nextval, 2, '7번글', '#태그실험', 2);
+insert	into tb_Around_My_Favorite (My_Seq, User_Seq, My_Content, My_Hashtag, My_Feeling)
+		values (s_Around_My_Favorite.nextval, 2, '8번글', '#태그실험', 5);
+insert	into tb_Around_My_Favorite (My_Seq, User_Seq, My_Content, My_Hashtag, My_Feeling)
+		values (s_Around_My_Favorite.nextval, 4, '9번글', '#태그실험', 5);
+insert	into tb_Around_My_Favorite (My_Seq, User_Seq, My_Content, My_Hashtag, My_Feeling)
+		values (s_Around_My_Favorite.nextval, 2, '10번글', '#태그실험', 5);
+insert	into tb_Around_My_Favorite (My_Seq, User_Seq, My_Content, My_Hashtag, My_Feeling)
+		values (s_Around_My_Favorite.nextval, 2, '11번글', '#태그실험', 5);
+
 
