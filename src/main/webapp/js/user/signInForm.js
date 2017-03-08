e07fb8f5e04e9a473d76275a02599a4ff867053d
hojohe <hojohe@nate.com>
2017-03-08 17:08:21 +0900

    자동로그인 기능 완료

--- src/main/webapp/js/user/signInForm.js
+++ src/main/webapp/js/user/signInForm.js
@@ -1,3 +1,39 @@
+var arrStrg=[];
+var objStrg={};
+$(function() {
+	
+	for (var i = 0; i < localStorage.length; i++) {
+		var key = localStorage.key(i);
+		var value = localStorage.getItem(key);
+		console.log(key+ ":" + value);
+		
+		if(key == "id") {
+			objStrg.id = value; 
+		}
+		if(key == "key"){
+			objStrg.key = value;
+		}
+	}
+	
+	arrStrg.push(objStrg);
+	
+	console.dir(arrStrg)
+
+	if(arrStrg != "") {
+		
+		var id = arrStrg[0].id;
+		var key = arrStrg[0].key;
+		
+		if(key) {
+			easySign("auto", "kakao", id, key);
+		} else {
+			easySign("auto", "google", id);
+		}
+		
+	}
+		
+
+
 var googleUser = {};
 var startApp = function() {
 	gapi.load('auth2', function(){
@@ -13,13 +49,19 @@ function attachSignin(element) {
 	console.log(element.id);
     auth2.attachClickHandler(element, {},
     function(googleUser) {
-    	easySign("google", googleUser.getBasicProfile().getEmail());
+    	easySign("direct", "google", googleUser.getBasicProfile().getEmail());
     }, function(error) {
     	alert(JSON.stringify(error, undefined, 2));
     });
 }
 
 w3IncludeHTML();
+
+
+startApp();
+
+});
+
 
 Kakao.init('a2df2dc9167541f9eb937ed24a7e8133');
 
@@ -51,7 +93,7 @@ function loginWithKakao() {
 				      		return false
 				    	}
 				    	
-						easySign("kakao", inputValue, res.id);
+						easySign("direct", "kakao", inputValue, res.id);
 				    	
 				    	
 					});
@@ -69,28 +111,24 @@ function loginWithKakao() {
 	});
 };
 
-function easySign(type, id, key) {
-	
-	var dataObj;
-	
-	if(type == "google") {
-		dataObj = {
-				type : type,
-				userId : id
-			}
-	}
-	if(type == "kakao") {
-		dataObj = {
-				type : type,
-				userId : id,
-				userKey: key
-		}
-	}
-	
 
+/*
+ * type : 간편 로그인 종류(kakao, google)
+ * id : email계정
+ * key: 카카오에서 주는 key값
+ * opt: 로컬 저장소 정보로 자동로그인 또는 다이렉트로그인 (auto, direct)
+ * */
+function easySign(opt, type, id, key) {
+	
+	if(key == undefined) key = "NULL";
+	
 	$.ajax({
 		url : "../../user/easySign.do",
-		data : dataObj,
+		data : {
+			type : type,
+			userId : id,
+			userKey: key
+		},
 		dataType : "json",
 		type: "post"
 	}).done(function(result) {
@@ -100,23 +138,32 @@ function easySign(type, id, key) {
 		var title;
 
 		if(result.msg == "signUp") {
+			
 			msg = "회원가입에 성공하였습니다.";
 			opt = "success";
 			title = "Success!";
+			
+			localStorage.setItem("id", id);
+			localStorage.setItem("key", key);
+			
 		} else if (result.msg == "info") {
 			msg = "사용중인 계정은 "+ result.user.userId +" 입니다. 다시 입력해주세요."
 			opt = "fail";
 			title = "Info";
 			
 		} else {
+			
 			msg = "로그인에 성공하였습니다.";
 			opt = "success";
 			title = "Success!";
+			
+			localStorage.setItem("id", id);
+			localStorage.setItem("key", key);
 		}
 		
 		// 로그인 또는 회원가입
 		if(result.msg != "info") {
-			swal(title, msg, opt);
+			if(opt != "auto")	swal(title, msg, opt);
 			
 			timer = setInterval(function () {
 				if(timer != null) location.href="../../view/main/main.html";		    	
@@ -125,11 +172,8 @@ function easySign(type, id, key) {
 			
 		// 카카오 로그인의 경우 회원가입된 계정과 동일해야 로그인 성공!
 		} else {
-			swal.showInputError(msg);
+			if(opt != "auto")	swal.showInputError(msg);
 		}
 		
 	});
 }
-
-
-startApp();
