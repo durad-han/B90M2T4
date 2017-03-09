dd22defff3ea82e05501b1e5485ba27679d22ea5
uruga00 <uruga00@gmail.com>
2017-03-09 12:12:07 +0900

    insertCard

--- src/main/webapp/js/card/insertCardForm.js
+++ src/main/webapp/js/card/insertCardForm.js
@@ -12,62 +12,62 @@ $(document).ready(function() {
 	});
 });
 
+// 해시태그 팝업 오픈
 function popupOpen() {
 	var popUrl = "insertCardFormPopup.html";
-	var popOption = "width=400, height=270, resizable=yes, scrollbars=yes, status=no";
+    var popOption = "width=400, height=270, resizable=yes, scrollbars=yes, status=no";
 	window.open(popUrl, "", popOption);
 }
 
-
+// 카드 작성
 $("#cardInsert").submit(function() {
-	
-//	if ($("#cardLongitude").val() == null) {
-//		alert("현재 위치를 조회해주세요 ^^");
-//		$("#cardLongitude").focus();
-//		return false;
-//	}
-	if ($("#cardContent").val() == null) {
-		alert("당신의 이야기를 입력해주세요 ^^");
-		$("#cardContent").focus();
-		return false;
-	}
-	if ($("#cardHashtag").val() == null) {
-		alert("해시태그를 1개 이상 입력해주세요 ^^");
-		$("#cardHashtag").focus();
-		return false;
-	}
-	console.log($("input[name=cardImg]").val());
-	
-	var fd = new FormData();
+    
+//  if ($("#cardLongitude").val() == null) {
+//      alert("현재 위치를 조회해주세요 ^^");
+//      $("#cardLongitude").focus();
+//      return false;
+//  }
+  if ($("#cardContent").val() == null) {
+      alert("당신의 이야기를 입력해주세요 ^^");
+      $("#cardContent").focus();
+      return false;
+  }
+  if ($("#cardHashtag").val() == null) {
+      alert("해시태그를 1개 이상 입력해주세요 ^^");
+      $("#cardHashtag").focus();
+      return false;
+  }
+  console.log($("input[name=cardImg]").val());
+  
+  var fd = new FormData();
 
-	if($("input[name=cardImg]").val()) {
-		var files = $("input[name=cardImg]")[0].files;
-		for (var i = 0 ; i < files.length ; i++) {
-			fd.append("cardImgPath", files[i]);
-		}		
-	}
-	
-	fd.append("cardContent", $("[name=cardContent]").val());
-	fd.append("cardFeeling", $("input[name=cardFeeling]:checked").val());
-	fd.append("cardHashtag", $("input[name=cardHashtag]").val());
-	fd.append("cardLongitude", $("input[name=cardLongitude]").val());
-	fd.append("cardLatitude", $("input[name=cardLatitude]").val());
-	
-	$.ajax({
-		url: "/b90m2t4/card/insert.do",
-		type: "post",
-		data: fd,
-		dataType: "html",
-		processData: false,
-		contentType: false
-	})
-	.done (function (result) {
-		alert(msg);
-		location.href = "/b90m2t4/main/main.do";
-	})
-	.fail (function (jqXhr, textStatus, errorText) {
-		alert("에러발생 : " + errorText);
-	});	
-	return false;
+  if($("input[name=cardImg]").val()) {
+      var files = $("input[name=cardImg]")[0].files;
+      for (var i = 0 ; i < files.length ; i++) {
+          fd.append("cardImgPath", files[i]);
+      }        
+  }
+  
+  fd.append("cardContent", $("[name=cardContent]").val());
+  fd.append("cardFeeling", $("input[name=cardFeeling]:checked").val());
+  fd.append("cardHashtag", $("input[name=cardHashtag]").val());
+  fd.append("cardLongitude", $("input[name=cardLongitude]").val());
+  fd.append("cardLatitude", $("input[name=cardLatitude]").val());
+  
+  $.ajax({
+      url: "/b90m2t4/card/insert.do",
+      type: "post",
+      data: fd,
+      dataType: "html",
+      processData: false,
+      contentType: false
+  })
+  .done (function (result) {
+      alert("msg");
+      location.href="../../view/main/main.html";
+  })
+  .fail (function (jqXhr, textStatus, errorText) {
+      alert("에러발생 : " + errorText);
+  });    
+  return false;
 });
-
--- src/main/webapp/js/card/insertCardFormPopup.js
+++ src/main/webapp/js/card/insertCardFormPopup.js
@@ -1,7 +1,7 @@
 /**
  * 
  */
-
+// 글자수 제한 및 글자수 표시
 $("#hashtagInput").keyup( function(e) {
 	if (e.keyCode == 13) {
 		opener.document.getElementById("hashtagText").value += " ";
@@ -19,9 +19,17 @@ $("#hashtagInput").keyup( function(e) {
 		var text = "";
 		if (list.length != 0) {
 			for (var i = 0; i < list.length; i++) {
-				text += list[i].hashtagContent + "<br>";
+				text += "<button type='button' id='b" + i + "' onclick='clickEvent(\"#" + list[i].hashtagContent + "\")'>#"
+				+ list[i].hashtagContent + "</button><br>";
 			}
 		$("#tagList").html(text);
 		};
 	});
 });
+
+function clickEvent(value) {
+	$("#hashtagInput").val(value);
+	opener.document.getElementById("hashtagText").value += " ";
+	opener.document.getElementById("hashtagText").value += $("#hashtagInput").val();
+	self.close();
+}
